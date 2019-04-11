114344b94401f93b77fb1c9f145970c372fbb8a5
Sachin Mahajan <sachinm78@gmail.com>
2019-04-11 17:09:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,6 +43,15 @@ class Dog
     dog
   end
   
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE id = ?
+    SQL
+
+    DB[:conn].execute(sql, id).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
   
 
 end
