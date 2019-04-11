18c587f2f75a562a0e4f251c4cf8d28acf8e2e6f
Sachin Mahajan <sachinm78@gmail.com>
2019-04-11 17:11:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,14 +68,13 @@ class Dog
       WHERE name = ? AND breed = ?
       SQL
 
-
       dog = DB[:conn].execute(sql, name, breed).first
 
-      if dog
-        new_dog = self.new_from_db(dog)
-      else
-        new_dog = self.create({:name => name, :breed => breed})
-      end
+        if dog
+          new_dog = self.new_from_db(dog)
+        else
+          new_dog = self.create({:name => name, :breed => breed})
+        end
       new_dog
   end
 
