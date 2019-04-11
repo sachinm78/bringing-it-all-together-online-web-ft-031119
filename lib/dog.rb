ba264d87fcade53b5d0c1ea284551d67395f1e7c
Sachin Mahajan <sachinm78@gmail.com>
2019-04-11 17:04:17 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,27 @@
+class Dog
+  attr_accessor :id, :name, :breed
+
+  def initialize(attributes)
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    self.id ||= nil
+  end
+
+  def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS dogs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        breed TEXT
+      )
+      SQL
+
+      DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = <<-SQL
+      DROP TABLE dogs
+      SQL
+
+    DB[:conn].execute(sql)
+  end
